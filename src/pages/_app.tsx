922820924b9b5566f05fd33269882e90aca37978
Joaquin Lopez <joaquinllopezzz@gmail.com>
2023-04-03 15:28:51 -0400

    adding chakra theming and design, finished navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,46 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+import "@fontsource/raleway/700.css";
+import "@fontsource/raleway/500.css";
+import "@fontsource/playfair-display/700.css";
+import "@fontsource/playfair-display/900.css";
+
+// 2. Call `extendTheme` and pass your custom values
+const theme = extendTheme({
+  fonts: {
+    heading: `'Playfair Display', sans-serif`,
+    body: `'Raleway', sans-serif`,
+  },
+  colors: {
+    brand: {
+      darkBlue: "#2E5B6F",
+      900: "#1a202c",
+    },
+  },
+
+  styles: {
+    global: {
+      body: {
+        bg: "white",
+      },
+    },
+  },
+  config: {},
+  useCustomProperties: true,
+  keyframes: {
+    fadeIn: {
+      "0%": { opacity: 0 },
+      "100%": { opacity: 1 },
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-return (
-    <ChakraProvider>
+  return (
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
-
+  );
 }
